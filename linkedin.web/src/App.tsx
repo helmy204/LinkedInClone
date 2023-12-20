28c637ddb2064502133d1266376a704a008653c2
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2023-12-20 14:55:38 +0100

    frontend header design

--- linkedin.web/src/App.tsx
+++ linkedin.web/src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './login/Login';
+import Header from './components/Header';
+import Home from './home/Home';
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />
+    },
+    {
+      path: "/home",
+      element: [<Header />, <Home />]
+    }
+  ]);
+
   return (
-    <div className="App">
-      Hello!
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
--- linkedin.web/src/index.tsx
+++ linkedin.web/src/index.tsx
@@ -1,30 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Login from "./login/Login"
-
-const router = createBrowserRouter([
-  // {
-  //   path: "/",
-  //   element: <App />
-  // },
-  {
-    // path: "/login",
-    path: "/",
-    element: <Login />
-  },
-]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
