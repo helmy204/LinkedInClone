a59093d42cd959d6bf32320a55abff2e233442e9
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2024-01-07 22:18:10 +0100

    frontend homepage left side ArtCard

--- linkedin.web/src/home/Home.tsx
+++ linkedin.web/src/home/Home.tsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import LeftSide from "./components/LeftSide";
+import Main from "./components/Main";
+import RightSide from "./components/RightSide";
 
 const Home = () => {
     return (
@@ -10,6 +13,11 @@ const Home = () => {
                     </h5>
                     <p>Find talented pros in record time with upwork and keep business moving.</p>
                 </Section>
+                <Layout>
+                    <LeftSide />
+                    <Main />
+                    <RightSide />
+                </Layout>
             </Content>
         </Container>
     );
@@ -51,6 +59,22 @@ const Section = styled.div`
     }
 
     @media (max-width: 768px) {
+        flex-direction: column;
+        padding: 0 5px;
+    }
+`;
+
+const Layout = styled.div`
+    display: grid;
+    grid-template-areas: "leftside main rightside";
+    grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px,7fr);
+    column-gap: 25px;
+    row-gap: 25px;
+    /* grid-template-rows: auto; */
+    margin: 25px 0;
+
+    @media (max-width: 768px) {
+        display: flex;
         flex-direction: column;
         padding: 0 5px;
     }
