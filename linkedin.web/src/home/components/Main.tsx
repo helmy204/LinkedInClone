c0af9c8e4bb005d17fe6880086c97bd1692d0afb
ahmedhelmy204 <ahmedhelmy204@hotmail.com>
2024-01-08 00:37:51 +0100

    frontend homepage share box

--- linkedin.web/src/home/components/Main.tsx
+++ linkedin.web/src/home/components/Main.tsx
@@ -1,13 +1,113 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Main = () => {
-    return (
-        <Container>Main</Container>
-    )
-}
+  return (
+    <Container>
+      <ShareBox>
+        Share
+        <div>
+          <img src="https://thispersondoesnotexist.com/" alt="" />
+          <button>Start a post</button>
+        </div>
+        <div>
+          <button>
+            <img src="/imgs/photo-icon.svg" alt="" />
+            <span>Photo</span>
+          </button>
+          <button>
+            <img src="/imgs/video-icon.svg" alt="" />
+            <span>Video</span>
+          </button>
+          <button>
+            <img src="/imgs/event-icon.svg" alt="" />
+            <span>Event</span>
+          </button>
+          <button>
+            <img src="/imgs/article-icon.svg" alt="" />
+            <span>Write article</span>
+          </button>
+        </div>
+      </ShareBox>
+    </Container>
+  );
+};
 
 const Container = styled.div`
   grid-area: main;
+`;
+
+const CommonCard = styled.div`
+  text-align: center;
+  overflow: hidden;
+  margin-bottom: 8px;
+  background-color: #fff;
+  border-radius: 5px;
+  position: relative;
+  border: none;
+  box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+`;
+
+// TODO: rename to PostBox
+const ShareBox = styled(CommonCard)`
+  display: flex;
+  flex-direction: column;
+  color: #958b7b;
+  margin: 0 0 8px;
+  background: white;
+
+  div {
+    button {
+      outline: none;
+      color: rgba(0, 0, 0, 0.6);
+      font-size: 14px;
+      line-height: 1.5;
+      min-height: 48px;
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+    }
+
+    &:first-child {
+      display: flex;
+      align-items: center;
+      padding: 8px 16px 0px 16px;
+
+      img {
+        width: 48px;
+        border-radius: 50%;
+        margin-right: 8px;
+      }
+
+      button {
+        margin: 4px 0;
+        flex-grow: 1;
+        border: 1px solid rgba(0, 0, 0, 0.15);
+        border-radius: 35px;
+        padding-left: 16px;
+        background-color: white;
+        text-align: left;
+      }
+    }
+
+    &:nth-child(2) {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      padding-bottom: 4px;
+
+      button {
+        img {
+          margin: 0 4px 0 -2px;
+        }
+
+        span {
+          color: #70b5f9;
+        }
+      }
+    }
+  }
 `;
 
 export default Main;
